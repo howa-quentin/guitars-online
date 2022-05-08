6755293ab90cb04f23bf7e47932486277164e4c2
Quentin Howa <quentinhowa@Quentins-MacBook-Pro.local>
2022-05-07 19:40:56 -0600

    nav

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include_once("database.php"); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/jquery-3.6.0.min.js"></script>
+	<script src="js/bootstrap.bundle.js"></script>
+	<script src="js/script.js" defer></script>
+
+	<title>Guitars Online</title>
+</head>
+<body>
+	<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+		<a href="#" class="navbar-brand">Guitars Online</a>
+		<button class="navbar-toggler" data-toggle="collapse" data-target="#main-nav">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div id="main-nav" class="collapse navbar-collapse">
+			<ul class="navbar-nav justify-content-around">
+				<li class="nav-item"><a class="nav-link" href="#">Guitars</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Amps</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Cart</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Account</a></li>
+			</ul>
+		</div>
+	</nav> 
+</body>
+</html>
